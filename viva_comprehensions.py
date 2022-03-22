d40099b485591848d68040eb54884f8cf437af7d
Joseph Kowalski <jkjr25@gmail.com>
2022-03-21 20:39:01 -0400

    First one done

--- viva_comprehensions.py
+++ viva_comprehensions.py
@@ -13,12 +13,19 @@ def gen_list(start: int, stop: int, parity: Parity) -> List[int]:
     what this method was supposed to do. Hey if you do, maybe you could do some good in this world by
     updating this here docstring to something useful.
 
-    :param start:
-    :param stop:
-    :param parity:
-    :return:
+    :param start: integer number to start
+    :param stop: integer number to stop
+    :param parity: determines whether odd or even values are returned
+    :return: list of odd or even numbers in start stop range
     """
-    pass
+    # list_of = []
+    # for value in range(start, stop):
+    #     if parity == parity.EVEN and value % 2 == 0:
+    #         list_of.append(value)
+    #     elif parity == parity.ODD and value % 2 != 0:
+    #         list_of.append(value)
+    # return list_of
+    return [value for value in range(start, stop) if (parity == parity.EVEN and value % 2 == 0) or (parity == parity.ODD and value % 2 != 0)]
 
 
 def gen_dict(start: int, stop: int, strategy: Callable) -> Dict:
